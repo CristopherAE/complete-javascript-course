57aa6d863f534ea79d19dfdce9dcf21a1520e85d
Cristopher Esquivel <33710623+CristopherAE@users.noreply.github.com>
2024-04-03 01:23:44 -0600

    Add lesson 209 in section 14 of the course
    The lesson added corresponds to the video titled "Constructor Functions
    and the new Operator".

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+/* ---- Constructor Functions and the new Operator ------------------------------------- */
+// We can create constructor functions with function declarations or funtion expressions, but
+// not with arrow functions, because arrow functions doesn't have a "this" keyword defined.
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // NEVER create a method inside of a constructor function. If we want to add a method to
+  // an object we use prototype inheritance.
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+// The "new" keyword does four things behind the scenes:
+//   1. Creates a new empty object {}.
+//   2. The function is called, and the "this" keyword will be set to the new created object.
+//   3. The newly created object will be linked to a prototype.
+//   4. Finally, the function automatically returns the object created.
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 2015);
+console.log(matilda, jack);
+
+// We can know if an object is an instance of a constructor function using the
+// "instanceof" operator
+console.log(jack instanceof Person); // true
+const jay = 'Jay';
+console.log(jay instanceof Person); // false
